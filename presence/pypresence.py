9eb62d86130f9849bdfc8479a3036be086d09f43
Awaiten <100166635+Awaiten@users.noreply.github.com>
2022-09-28 17:59:37 -0400

    Create pypresence.py

---
+++ presence/pypresence.py
@@ -0,0 +1,13 @@
+from pypresence import Presence
+import time
+
+client_id = '2399023920329030992'  # Fake ID, put your real one here
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect()  # Start the handshake loop
+
+print(RPC.update(state="...", details="...", large_image="...", small_image="...", start="...", buttons=[
+      {"label": "...", "url": "..."}, {"label": "...", "url": "..."}], party_size=[2, 3], party_id="...",))  # Set the presence
+
+
+while True:  # The presence will stay on as long as the program is running
+    time.sleep(15)  # Can only update rich presence every 15 seconds
