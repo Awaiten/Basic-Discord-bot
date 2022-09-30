97690810b91b4129d350f56865603442047a11bb
Awaiten <100166635+Awaiten@users.noreply.github.com>
2022-09-30 16:52:52 -0400

    Update Javascript.js

--- bot/discordjs/Javascript.js
+++ bot/discordjs/Javascript.js
@@ -1,1 +1,11 @@
-fire = ["i don't actully know any javascript so  yeah......"]
+
+
+fire = ["i don't actully know any javascript so  yeah......",
+       "Second line",
+       "Char",
+       "Why",
+       "cancel",
+       "continue",
+       "exit",
+       "no more",
+       "Procede"]
